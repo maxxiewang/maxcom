654ed124bfc3d05cee5f21d63ac4d7abf6654642
wangfei_pro <51104860@163.com>
2022-01-06 22:32:11 +0800

    C

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import MxButton from './button'
+import { render, fireEvent } from '@testing-library/react'
+import MxButton, { ButtonProps, ButtonSize, ButtonType } from './button'
 
 // test('Button test', () => {
 //   const wrapper = render(<MxButton>TestBu</MxButton>)
@@ -8,18 +8,38 @@ import MxButton from './button'
 //   expect(element).toBeTruthy()
 //   expect(element).toBeInTheDocument() // 比Truthy好点
 // })
+const defaultProps = {
+  //! 这个onClick名是必须的，传个别的不行
+  onClick: jest.fn(),
+}
+
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: 'KGB',
+}
 
 // 先把case作一个分类
 
 describe('test Button component', () => {
   it('should render the correct default button', () => {
-    const wrapper = render(<MxButton>TestBu</MxButton>)
+    const wrapper = render(<MxButton {...defaultProps}>TestBu</MxButton>)
     const element = wrapper.getByText('TestBu')
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('BUTTON')
     expect(element).toHaveClass('btn btn-default')
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
   })
-  it('based on different props', () => {})
-  it('link', () => {})
+
+  it('based on different props', () => {
+    const wrapper = render(<MxButton {...testProps}>TestBu</MxButton>)
+    const element = wrapper.getByText('TestBu')
+    expect(element).toBeInTheDocument()
+    expect(element).toHaveClass('btn-primary KGB btn-lg')
+  })
+  it('测试是否是a link', () => {
+    // const wrapper = render()
+  })
   it('test disabled button', () => {})
 })
